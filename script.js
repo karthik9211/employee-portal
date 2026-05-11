afc6c085d375b2cd6122c8f881f0af8b83787e09
kar1508 <karthikeyar1508@gmail.com>
2026-05-11 18:55:05 -0400

    Added dark mode feature

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Portal Loaded");
+
+const button = document.getElementById("darkBtn");
+
+button.addEventListener("click", function() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+});
